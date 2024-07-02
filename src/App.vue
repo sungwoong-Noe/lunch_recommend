f93dbd68399ef34d484fb4b64544e6a998e33834
sungwoongNoh <asdz453@gmail.com>
2024-07-02 15:01:02 +0900

    feat: 카카오 API 설정

--- src/App.vue
+++ src/App.vue
@@ -1,99 +1,13 @@
 <script setup lang="ts">
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
+
+import {useKakao} from "vue3-kakao-maps";
+
+useKakao(import.meta.env.VITE_KAKAO_APP_KEY,['services']);
+
+
 </script>
-
 <template>
-  <template>
-    <div class="common-layout">
-      <el-container>
-        <el-header>Header</el-header>
-        <el-container>
-          <el-aside width="200px">Aside</el-aside>
-          <el-container>
-            <el-main>Main</el-main>
-            <el-footer>Footer</el-footer>
-          </el-container>
-        </el-container>
-      </el-container>
-    </div>
-  </template>
-<!--  <header>-->
-<!--    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />-->
-
-<!--    <div class="wrapper">-->
-<!--      <HelloWorld msg="You did it!" />-->
-
-<!--      <nav>-->
-<!--        <RouterLink to="/">Home</RouterLink>-->
-<!--        <RouterLink to="/about">About</RouterLink>-->
-<!--      </nav>-->
-<!--    </div>-->
-<!--  </header>-->
-
-<!--  <RouterView />-->
+  <el-button>I am ElButton</el-button>
 </template>
 
-<!--<style scoped>-->
-<!--header {-->
-<!--  line-height: 1.5;-->
-<!--  max-height: 100vh;-->
-<!--}-->
 
-<!--.logo {-->
-<!--  display: block;-->
-<!--  margin: 0 auto 2rem;-->
-<!--}-->
-
-<!--nav {-->
-<!--  width: 100%;-->
-<!--  font-size: 12px;-->
-<!--  text-align: center;-->
-<!--  margin-top: 2rem;-->
-<!--}-->
-
-<!--nav a.router-link-exact-active {-->
-<!--  color: var(&#45;&#45;color-text);-->
-<!--}-->
-
-<!--nav a.router-link-exact-active:hover {-->
-<!--  background-color: transparent;-->
-<!--}-->
-
-<!--nav a {-->
-<!--  display: inline-block;-->
-<!--  padding: 0 1rem;-->
-<!--  border-left: 1px solid var(&#45;&#45;color-border);-->
-<!--}-->
-
-<!--nav a:first-of-type {-->
-<!--  border: 0;-->
-<!--}-->
-
-<!--@media (min-width: 1024px) {-->
-<!--  header {-->
-<!--    display: flex;-->
-<!--    place-items: center;-->
-<!--    padding-right: calc(var(&#45;&#45;section-gap) / 2);-->
-<!--  }-->
-
-<!--  .logo {-->
-<!--    margin: 0 2rem 0 0;-->
-<!--  }-->
-
-<!--  header .wrapper {-->
-<!--    display: flex;-->
-<!--    place-items: flex-start;-->
-<!--    flex-wrap: wrap;-->
-<!--  }-->
-
-<!--  nav {-->
-<!--    text-align: left;-->
-<!--    margin-left: -1rem;-->
-<!--    font-size: 1rem;-->
-
-<!--    padding: 1rem 0;-->
-<!--    margin-top: 1rem;-->
-<!--  }-->
-<!--}-->
-<!--</style>-->
